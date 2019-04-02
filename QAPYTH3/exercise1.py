fae82c3c0c01354d75485031aae47ebf27659cbc
Dong Ma <winterma.dong@gmail.com>
2019-04-02 12:55:49 +0800

    Exercise 1 for QAPYTH3 Training

---
+++ QAPYTH3/exercise1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+first_name = "Dong"
+last_name = "Ma"
+
+print(first_name, last_name, sep="")
+
+number = input("please input the number (1 -52):")
+
+filename = "BMP" + number + ".GIF"
+
+print(filename)
