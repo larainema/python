5608642008804d3fd62ffd5385b4d92b8a4b2b97
Dong Ma <winterma.dong@gmail.com>
2019-04-07 11:39:28 +0800

    Exercise 3.1 for QAPYTH3 Training

--- QAPYTH3/exercise3.py
+++ QAPYTH3/exercise3.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
-import os, glob
+import sys, os, glob
 
-homepath = os.environ['HOME']
+if sys.platform == 'win32':
+   homepath = os.environ['HOMEPATH']
+else:
+   homepath = os.environ['HOME']
 print(homepath)
 
-filenames = glob.glob(homepath, '**', True)
+pattern = os.path.join(homepath, '*')
+print(pattern)
+
+filenames = glob.glob(pattern)
+print(filenames)
+
+for filename in filenames:
+    size = os.path.getsize(filename)
+    #print(size)
+    if size > 0:
+       print('File:', os.path.basename(filename))
+       print('Size:', size, 'bytes')
